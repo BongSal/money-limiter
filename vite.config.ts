5dc60f63d710c00f9a0ff15d2043a182593c4b73
Kosal Koeng <k-koeung@beniten.com>
2024-11-07 15:56:28 +0700

    Serve pwa.

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,128 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import autoprefixer from 'autoprefixer'
-import tailwind from 'tailwindcss'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import autoprefixer from "autoprefixer";
+import tailwind from "tailwindcss";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   css: {
-  postcss: {
-    plugins: [tailwind(), autoprefixer()],
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
   },
-},
   plugins: [
     vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Money Limiter",
+        short_name: "Money Limiter",
+        description:
+          "Money Limiter helps you take control of your finances by setting spending limits and tracking your expenses. Stay on top of your budget with real-time notifications and make smarter financial decisions. Start managing your money with ease today!",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/icon-128x128.png",
+            sizes: "128x128",
+            type: "image/png",
+          },
+          {
+            src: "/icon-128x128.png",
+            sizes: "128x128",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "/icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+          },
+          {
+            src: "/icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
